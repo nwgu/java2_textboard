201db71c876aa785496a9cecaba304cbab012341
nwgu <nwgu99@gmail.com>
2025-04-05 22:00:09 +0900

    /post/article 명령어 입력 시 제목 및 내용 입력받기

--- src/java2/Main.java
+++ src/java2/Main.java
@@ -14,14 +14,29 @@ public class Main {
 
 			String request = sc.next();
 
-			if (request.equals("/get/help")) {
+			if (request.equals("/post/article")) {
+
+				sc.nextLine(); // 버퍼 비우기
+
+				System.out.print("제목 : ");
+				String title = sc.nextLine(); // 공백 및 엔터를 가져감.
+
+				System.out.print("내용 : ");
+				String body = sc.nextLine();
+
+			}
+
+			else if (request.equals("/get/help")) {
 
 				System.out.println("=== 도움말 ===");
 				System.out.println("/exit - 프로그램 종료");
 				System.out.println("/get/help - 도움말 출력");
 
+				System.out.println("/post/article - 게시글 작성");
+
 			} else if (request.equals("/exit")) {
 				System.out.println("프로그램 종료");
+				sc.close();
 				break;
 			}
 		}
