e54d3dec0daa9fa490a3268b80a0f64397691abb
nwgu <nwgu99@gmail.com>
2025-04-05 21:54:18 +0900

    while 문을 통해 데이터 무한히 입력받기

--- src/java2/Main.java
+++ src/java2/Main.java
@@ -1,7 +1,18 @@
 package java2;
 
+import java.util.Scanner;
+
 public class Main {
 	public static void main(String[] args) {
-		
+
+		Scanner sc = new Scanner(System.in);
+
+		System.out.println("텍스트 게시판 시작");
+
+		while (true) {
+			System.out.print("명령어 : ");
+			
+			String request = sc.next();
+		}
 	}
 }
