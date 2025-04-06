e790ca753dd84a91e7a92118bd97a587dea50e41
nwgu <nwgu99@gmail.com>
2025-04-06 22:54:17 +0900

    Article class 주석 설명 보완

--- src/vo/Article.java
+++ src/vo/Article.java
@@ -3,11 +3,17 @@ package vo; // vo --> Value Object 의 약자 (데이터를 담아 전달하는 
 // 게시글을 담당하는 클래스 
 public class Article {
 
+	// Article 클래스 안의 게시글의 속성들 (변수)
+	// 캡슐화를 통한 private 접근제어자로 작성
 	private int articleId;
 	private String regDate;
 	private String title;
 	private String body;
 
+	// 위의 필드 변수가 private 이기 때문에, 값을 세팅하고 가져오려면
+	// 아래 코드처럼 getter, setter 메서드를 만들어줘야 한다.
+	// 게터세터 쉽게 만들기 -> (오른쪽 클릭 -> source -> Generate Getter and Setter 클릭 -> select all -> generate)
+	
 	public int getArticleId() {
 		return articleId;
 	}
